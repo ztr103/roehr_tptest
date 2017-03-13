8f0f72d9c2c5f2a783b78ceb0f667ba2dd784856
Zack <Zack@ZROEHR>
2017-03-12 20:01:19 -0400

    Add project files.

---
+++ TPTest/CreateGrid.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geoprocessing;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geometry;
+
+
+
+namespace TPTest
+{
+    public class CreateGrid : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public CreateGrid()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            ArcMap.Application.CurrentTool = null;
+
+            //Do Stuff
+            CreateGRID("c:/TestOutput", "InspectionGRID", 0.05, "test_points");
+
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+
+        public void CreateGRID(string pathName, string fcName, double gridSize, string templateLayer)
+        {
+
+            //Get Extents of template FC
+
+            IMxDocument pMxDoc;
+            pMxDoc = (IMxDocument)ArcMap.Application.Document;
+
+            IMap pMap;
+            pMap = pMxDoc.FocusMap;
+
+            IEnumLayer pEnumLayer;
+            pEnumLayer = pMap.Layers;
+
+            ILayer pLayer;
+            pLayer = pEnumLayer.Next();
+
+            string strName;
+            double Xmin = 0;
+            double Xmax = 0;
+            double Ymin = 0;
+            double Ymax = 0;
+
+            while (pLayer != null)
+            {
+                strName = pLayer.Name;
+
+                if (strName == templateLayer)
+                {
+                    IEnvelope templateEnv;
+                    templateEnv = (IEnvelope)pLayer.AreaOfInterest;
+
+                    //MessageBox.Show(strName, "Template Layer Found", MessageBoxButtons.OK);
+
+                    Xmin = templateEnv.XMin;
+                    Xmax = templateEnv.XMax;
+                    Ymin = templateEnv.YMin;
+                    Ymax = templateEnv.YMax;
+                }
+
+                pLayer = pEnumLayer.Next();
+            }
+
+            //Populate some values to feed geoprocess parameters
+            string originString = Xmin + " " + Ymax;
+            string yaxisString = Xmin + " " + (Ymax - gridSize);
+            string outputPath = pathName + "\\" + fcName + ".shp";
+
+            // Create the geoprocessor object
+            IGeoProcessor2 geoProcess;
+            geoProcess = (IGeoProcessor2)new GeoProcessor();
+
+            // Create an IVariantArray to hold the parameter values
+            IVariantArray geoParams = new VarArrayClass();
+
+            // Populate parameter values
+            geoParams.Add(outputPath); // Output Feature Class
+            geoParams.Add(originString); // Output Origin Coordinate
+            geoParams.Add(yaxisString); // Output Y Axis Coordinate
+            geoParams.Add(gridSize); // Output Cell Width
+            geoParams.Add(gridSize); // Output Cell Height
+            geoParams.Add("0"); // Number of Rows
+            geoParams.Add("0"); // Number of Columns
+            geoParams.Add(""); // Output Corner Coord
+            geoParams.Add("NO_LABELS"); // Output Lables
+            geoParams.Add(""); // Output Template - Xmin, Xmax, Ymin, Ymax, OR template dataset
+            geoParams.Add("POLYGON"); // Output Geometry Type
+
+            try
+            {
+                // Run geoprocessor
+                geoProcess.Execute("CreateFishnet", geoParams, null);
+
+            }
+            catch (System.Runtime.InteropServices.COMException ce)
+            {
+                MessageBox.Show("Error Code: "+ce.ErrorCode.ToString(), "COM Exception", System.Windows.Forms.MessageBoxButtons.OK);
+                return;
+            }
+
+}
+
+    }
+}
---
+++ TPTest/CreatePointFC.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.DataSourcesFile;
+using ESRI.ArcGIS.Geometry;
+
+namespace TPTest
+{
+    public class CreatePointFC : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public CreatePointFC()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            ArcMap.Application.CurrentTool = null;
+
+            //Do Stuff
+            CreatePointFClass("c:/TestOutput", "InspectionPoints");
+            AddLayer("c:/TestOutput", "InspectionPoints");
+
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+
+        public void CreatePointFClass(string pathName, string fcName)
+        {
+            IMxDocument pMxDoc;
+            pMxDoc = (IMxDocument)ArcMap.Application.Document;
+
+            IMap pMap;
+            pMap = pMxDoc.FocusMap;
+
+            IWorkspaceFactory pWSFactory;
+            pWSFactory = new ShapefileWorkspaceFactory();
+
+            IFeatureWorkspace pFWorkspace;
+            pFWorkspace = (IFeatureWorkspace)pWSFactory.OpenFromFile(pathName, ArcMap.Application.hWnd);
+
+            IFieldsEdit pFieldsEdit;
+            pFieldsEdit = (IFieldsEdit)new Fields();
+
+            IFieldEdit pIDField;
+            pIDField = (IFieldEdit)new Field();
+            pIDField.Name_2 = "OID";
+            pIDField.Type_2 = esriFieldType.esriFieldTypeOID;
+            pIDField.Length_2 = 5;
+
+            IFieldEdit pCodeField;
+            pCodeField = (IFieldEdit)new Field();
+            pCodeField.Name_2 = "Feat_Code";
+            pCodeField.Type_2 = esriFieldType.esriFieldTypeInteger;
+
+            IFieldEdit pDescField;
+            pDescField = (IFieldEdit)new Field();
+            pDescField.Name_2 = "Feat_Desc";
+            pDescField.Type_2 = esriFieldType.esriFieldTypeString;
+            pDescField.Length_2 = 80;
+
+            IFieldEdit pShapeField;
+            pShapeField = (IFieldEdit)new Field();
+            pShapeField.Name_2 = "Shape";
+            pShapeField.Type_2 = esriFieldType.esriFieldTypeGeometry;
+
+            // Create Spatial Reference
+            ISpatialReferenceFactory3 pSpatRefGen;
+            pSpatRefGen = (ISpatialReferenceFactory3)new SpatialReferenceEnvironment();
+            ISpatialReference pSpatialRef;
+            pSpatialRef = pSpatRefGen.CreateSpatialReference(4326);
+
+            //Setup Geometry Definition Settings
+            IGeometryDef pGeomDef = new GeometryDef();
+            IGeometryDefEdit pGDefEdit = (IGeometryDefEdit)pGeomDef;
+            pGDefEdit.GeometryType_2 = esriGeometryType.esriGeometryPoint;
+            pGDefEdit.GridCount_2 = 1;
+            pGDefEdit.set_GridSize(0, 0.1);
+            pGDefEdit.SpatialReference_2 = pSpatialRef;
+
+            //Set Shape Field Geometry Definition
+            pShapeField.GeometryDef_2 = pGeomDef;
+
+            //Add Fields to Collection
+            pFieldsEdit.AddField(pIDField);
+            pFieldsEdit.AddField(pCodeField);
+            pFieldsEdit.AddField(pDescField);
+            pFieldsEdit.AddField(pShapeField);
+
+            pFWorkspace.CreateFeatureClass(fcName + ".shp", pFieldsEdit, null, null, esriFeatureType.esriFTSimple, "Shape", "");
+
+        }
+
+        public void AddLayer(string pathName, string fcName)
+        {
+            IMxDocument pMxDoc;
+            pMxDoc = (IMxDocument)ArcMap.Application.Document;
+
+            IMap pMap;
+            pMap = pMxDoc.FocusMap;
+
+            IFeatureLayer pFLayer;
+            pFLayer = new FeatureLayer();
+
+            IWorkspaceFactory pWSFactory;
+            pWSFactory = new ShapefileWorkspaceFactory();
+
+            IWorkspace pWorkspace;
+            pWorkspace = pWSFactory.OpenFromFile(pathName,ArcMap.Application.hWnd);
+
+            IFeatureWorkspace pFWorkspace;
+            pFWorkspace = (IFeatureWorkspace)pWorkspace;
+
+            IFeatureClass pFClass;
+            pFClass = pFWorkspace.OpenFeatureClass(fcName+".shp");
+
+            pFLayer.FeatureClass = pFClass;
+            pFLayer.Name = fcName;
+
+            pMap.AddLayer(pFLayer);
+
+            pMxDoc.UpdateContents();
+
+        }
+
+    }
+
+}
